16d29e882a31697eb4892240b23e27b123549232
Secra <niroman_11@mail.ru>
2025-01-27 20:57:12 +0300

    fix

---
+++ .gradle/8.10.2/dependencies-accessors/84b7a0c3f37c8a093887f8f90f1a2a0dbe31523b/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,493 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the {@code libs} extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final ConverterLibraryAccessors laccForConverterLibraryAccessors = new ConverterLibraryAccessors(owner);
+    private final EspressoLibraryAccessors laccForEspressoLibraryAccessors = new EspressoLibraryAccessors(owner);
+    private final ExtLibraryAccessors laccForExtLibraryAccessors = new ExtLibraryAccessors(owner);
+    private final JacksonLibraryAccessors laccForJacksonLibraryAccessors = new JacksonLibraryAccessors(owner);
+    private final Retrofit2LibraryAccessors laccForRetrofit2LibraryAccessors = new Retrofit2LibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Dependency provider for <b>activity</b> with <b>androidx.activity:activity</b> coordinates and
+     * with version reference <b>activity</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getActivity() {
+        return create("activity");
+    }
+
+    /**
+     * Dependency provider for <b>appcompat</b> with <b>androidx.appcompat:appcompat</b> coordinates and
+     * with version reference <b>appcompat</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getAppcompat() {
+        return create("appcompat");
+    }
+
+    /**
+     * Dependency provider for <b>constraintlayout</b> with <b>androidx.constraintlayout:constraintlayout</b> coordinates and
+     * with version reference <b>constraintlayout</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getConstraintlayout() {
+        return create("constraintlayout");
+    }
+
+    /**
+     * Dependency provider for <b>junit</b> with <b>junit:junit</b> coordinates and
+     * with version reference <b>junit</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getJunit() {
+        return create("junit");
+    }
+
+    /**
+     * Dependency provider for <b>lombok</b> with <b>org.projectlombok:lombok</b> coordinates and
+     * with version reference <b>lombok</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getLombok() {
+        return create("lombok");
+    }
+
+    /**
+     * Dependency provider for <b>material</b> with <b>com.google.android.material:material</b> coordinates and
+     * with version reference <b>material</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getMaterial() {
+        return create("material");
+    }
+
+    /**
+     * Dependency provider for <b>retrofit</b> with <b>com.squareup.retrofit2:retrofit</b> coordinates and
+     * with version reference <b>retrofit</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getRetrofit() {
+        return create("retrofit");
+    }
+
+    /**
+     * Group of libraries at <b>converter</b>
+     */
+    public ConverterLibraryAccessors getConverter() {
+        return laccForConverterLibraryAccessors;
+    }
+
+    /**
+     * Group of libraries at <b>espresso</b>
+     */
+    public EspressoLibraryAccessors getEspresso() {
+        return laccForEspressoLibraryAccessors;
+    }
+
+    /**
+     * Group of libraries at <b>ext</b>
+     */
+    public ExtLibraryAccessors getExt() {
+        return laccForExtLibraryAccessors;
+    }
+
+    /**
+     * Group of libraries at <b>jackson</b>
+     */
+    public JacksonLibraryAccessors getJackson() {
+        return laccForJacksonLibraryAccessors;
+    }
+
+    /**
+     * Group of libraries at <b>retrofit2</b>
+     */
+    public Retrofit2LibraryAccessors getRetrofit2() {
+        return laccForRetrofit2LibraryAccessors;
+    }
+
+    /**
+     * Group of versions at <b>versions</b>
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Group of bundles at <b>bundles</b>
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Group of plugins at <b>plugins</b>
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class ConverterLibraryAccessors extends SubDependencyFactory {
+
+        public ConverterLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>gson</b> with <b>com.squareup.retrofit2:converter-gson</b> coordinates and
+         * with version reference <b>retrofit</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getGson() {
+            return create("converter.gson");
+        }
+
+        /**
+         * Dependency provider for <b>jackson</b> with <b>com.squareup.retrofit:converter-jackson</b> coordinates and
+         * with version <b>1.9.0</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getJackson() {
+            return create("converter.jackson");
+        }
+
+    }
+
+    public static class EspressoLibraryAccessors extends SubDependencyFactory {
+
+        public EspressoLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>core</b> with <b>androidx.test.espresso:espresso-core</b> coordinates and
+         * with version reference <b>espressoCore</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getCore() {
+            return create("espresso.core");
+        }
+
+    }
+
+    public static class ExtLibraryAccessors extends SubDependencyFactory {
+
+        public ExtLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>junit</b> with <b>androidx.test.ext:junit</b> coordinates and
+         * with version reference <b>junitVersion</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getJunit() {
+            return create("ext.junit");
+        }
+
+    }
+
+    public static class JacksonLibraryAccessors extends SubDependencyFactory {
+        private final JacksonDatabindLibraryAccessors laccForJacksonDatabindLibraryAccessors = new JacksonDatabindLibraryAccessors(owner);
+
+        public JacksonLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>annotations</b> with <b>com.fasterxml.jackson.core:jackson-annotations</b> coordinates and
+         * with version reference <b>jacksonDatabind</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getAnnotations() {
+            return create("jackson.annotations");
+        }
+
+        /**
+         * Dependency provider for <b>core</b> with <b>com.fasterxml.jackson.core:jackson-core</b> coordinates and
+         * with version reference <b>jacksonDatabind</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getCore() {
+            return create("jackson.core");
+        }
+
+        /**
+         * Group of libraries at <b>jackson.databind</b>
+         */
+        public JacksonDatabindLibraryAccessors getDatabind() {
+            return laccForJacksonDatabindLibraryAccessors;
+        }
+
+    }
+
+    public static class JacksonDatabindLibraryAccessors extends SubDependencyFactory implements DependencyNotationSupplier {
+
+        public JacksonDatabindLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>databind</b> with <b>com.fasterxml.jackson.core:jackson-databind</b> coordinates and
+         * with version reference <b>jacksonDatabind</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> asProvider() {
+            return create("jackson.databind");
+        }
+
+        /**
+         * Dependency provider for <b>v2101</b> with <b>com.fasterxml.jackson.core:jackson-databind</b> coordinates and
+         * with version reference <b>jacksonDatabindVersion</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getV2101() {
+            return create("jackson.databind.v2101");
+        }
+
+    }
+
+    public static class Retrofit2LibraryAccessors extends SubDependencyFactory {
+        private final Retrofit2ConverterLibraryAccessors laccForRetrofit2ConverterLibraryAccessors = new Retrofit2ConverterLibraryAccessors(owner);
+
+        public Retrofit2LibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Group of libraries at <b>retrofit2.converter</b>
+         */
+        public Retrofit2ConverterLibraryAccessors getConverter() {
+            return laccForRetrofit2ConverterLibraryAccessors;
+        }
+
+    }
+
+    public static class Retrofit2ConverterLibraryAccessors extends SubDependencyFactory {
+
+        public Retrofit2ConverterLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Dependency provider for <b>jackson</b> with <b>com.squareup.retrofit2:converter-jackson</b> coordinates and
+         * with version <b>2.11.0</b>
+         * <p>
+         * This dependency was declared in catalog libs.versions.toml
+         */
+        public Provider<MinimalExternalModuleDependency> getJackson() {
+            return create("retrofit2.converter.jackson");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Version alias <b>activity</b> with value <b>1.8.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getActivity() { return getVersion("activity"); }
+
+        /**
+         * Version alias <b>agp</b> with value <b>8.6.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getAgp() { return getVersion("agp"); }
+
+        /**
+         * Version alias <b>appcompat</b> with value <b>1.6.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getAppcompat() { return getVersion("appcompat"); }
+
+        /**
+         * Version alias <b>constraintlayout</b> with value <b>2.1.4</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getConstraintlayout() { return getVersion("constraintlayout"); }
+
+        /**
+         * Version alias <b>converterJackson</b> with value <b>1.9.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getConverterJackson() { return getVersion("converterJackson"); }
+
+        /**
+         * Version alias <b>converterJacksonVersion</b> with value <b><latestversion></b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getConverterJacksonVersion() { return getVersion("converterJacksonVersion"); }
+
+        /**
+         * Version alias <b>espressoCore</b> with value <b>3.5.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getEspressoCore() { return getVersion("espressoCore"); }
+
+        /**
+         * Version alias <b>jacksonDatabind</b> with value <b>2.18.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJacksonDatabind() { return getVersion("jacksonDatabind"); }
+
+        /**
+         * Version alias <b>jacksonDatabindVersion</b> with value <b>2.18.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJacksonDatabindVersion() { return getVersion("jacksonDatabindVersion"); }
+
+        /**
+         * Version alias <b>junit</b> with value <b>4.13.2</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJunit() { return getVersion("junit"); }
+
+        /**
+         * Version alias <b>junitVersion</b> with value <b>1.1.5</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJunitVersion() { return getVersion("junitVersion"); }
+
+        /**
+         * Version alias <b>lombok</b> with value <b>1.18.36</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getLombok() { return getVersion("lombok"); }
+
+        /**
+         * Version alias <b>material</b> with value <b>1.10.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getMaterial() { return getVersion("material"); }
+
+        /**
+         * Version alias <b>retrofit</b> with value <b>2.11.0</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getRetrofit() { return getVersion("retrofit"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+        private final AndroidPluginAccessors paccForAndroidPluginAccessors = new AndroidPluginAccessors(providers, config);
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Group of plugins at <b>plugins.android</b>
+         */
+        public AndroidPluginAccessors getAndroid() {
+            return paccForAndroidPluginAccessors;
+        }
+
+    }
+
+    public static class AndroidPluginAccessors extends PluginFactory {
+
+        public AndroidPluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Plugin provider for <b>android.application</b> with plugin id <b>com.android.application</b> and
+         * with version reference <b>agp</b>
+         * <p>
+         * This plugin was declared in catalog libs.versions.toml
+         */
+        public Provider<PluginDependency> getApplication() { return createPlugin("android.application"); }
+
+    }
+
+}
